43b5d1322616d40c48a7b027a62ac39e9e5710c6
krealif <85614368+krealif@users.noreply.github.com>
2024-06-04 13:51:47 +0700

    add input mask

--- app/Forms/Components/NimInput.php
+++ app/Forms/Components/NimInput.php
@@ -9,6 +9,8 @@ class NimInput extends TextInput
     public function format(): static
     {
         return $this
+            ->placeholder('00/000000/SV/00000')
+            ->mask('99/999999/aa/99999')
             ->regex('/^\d{2}\/\d{6}\/[A-Z]{2}\/\d{5}$/');
     }
 }
