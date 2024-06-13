513d447f9442b795212eed40c0a7e5257ba11f7d
Luthfia39 <luthfianisaaz339@gmail.com>
2024-06-13 11:06:26 +0700

    Add template to seeder

--- database/seeders/TemplateSeeder.php
+++ database/seeders/TemplateSeeder.php
@@ -13,12 +13,15 @@ class TemplateSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->insertTemplate('Praktik Industri', 'PraktikIndustri');
+        $this->insertTemplate('Surat Tugas Praktik Industri', 'PraktikIndustri');
         $this->insertTemplate('Keterangan Aktif Kuliah', 'KeteranganAktifKuliah');
         $this->insertTemplate('Keterangan Alumni', 'KeteranganAlumni');
         $this->insertTemplate('Rekomendasi Beasiswa', 'RekomendasiBeasiswa');
         $this->insertTemplate('Magang PMMB', 'MagangPmmb');
         $this->insertTemplate('Pengantar Penelitian PA', 'PengantarPenelitian');
+        $this->insertTemplate('Pengantar Praktik Industri 1 Orang', 'PengantarPraktikIndustri1');
+        $this->insertTemplate('Pengantar Praktik Industri 2 Orang atau Lebih', 'PengantarPraktikIndustri2');
+        $this->insertTemplate('Permohonan Magang', 'PermohonanMagang');
     }
 
     protected function insertTemplate(string $name, string $class_name): void
