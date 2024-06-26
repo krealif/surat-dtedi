e40044c96feebb0884743062bb7b5326ae9a269f
krealif <85614368+krealif@users.noreply.github.com>
2024-06-26 23:56:14 +0700

    feat[page]: add heading and subheading

--- app/Filament/Resources/TemplateResource/Pages/ListTemplates.php
+++ app/Filament/Resources/TemplateResource/Pages/ListTemplates.php
@@ -8,4 +8,15 @@ use Filament\Resources\Pages\ListRecords;
 class ListTemplates extends ListRecords
 {
     protected static string $resource = TemplateResource::class;
+
+    public function getHeading(): string
+    {
+        $user = auth()->user();
+        return "Halo, {$user->name}";
+    }
+
+    public function getSubheading(): string
+    {
+        return 'Pilih salah satu template di bawah ini untuk mulai membuat surat';
+    }
 }
